de45517f86eb2b087fb95ac5bbeaa29a5f89a4bf
SanjarKeySkills <forest2004@mail.ru>
2023-07-04 22:00:02 +0600

    add test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Modal from "../src/components/Modal"
+import Test from "./components/test/Test";
 
 function App() {
   return (
     <div className="App">
       <Modal/>
+        <Test />
     </div>
   );
 }
---
+++ src/components/test/Test.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+    render() {
+        return (
+            <div className={test_elem}>
+                test
+            </div>
+        );
+    }
+}
+
+export default Test;
